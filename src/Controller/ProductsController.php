faaf1edb284408bb22ae1185de8a6e3f0bf7c90e
Michał Śnieżko <michalsniezko@gmail.com>
2025-06-29 20:29:52 +0200

    productsController

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,44 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 class ProductsController
 {
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
 
+        // 1. Deserialize json data into an Enquiry DTO
+        // 2. Pass the Enquiry into a promotions filter
+            // The appropriate promotion will be applied
+        // 3. Return the modified Enquiry
+
+        return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => 'UK',
+            'voucher_code' => 'OU812',
+            'request_date' => '2022-04-04',
+            'product_id' => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+
+    }
 }
